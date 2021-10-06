77c5596b72c869c0f03b0a286b3771ccbc7ea2a4
ChingHuanWang <85130393+ChingHuanWang@users.noreply.github.com>
2021-10-06 21:44:39 +0800

    Add files via upload

---
+++ DSnP/hw1/p1/test.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+// #include <boost/property_tree/json_parser.hpp>
+// #include <boost/property_tree/ptree.hpp>
+// #include "json.hpp"
+#include <string>
+#include <fstream>
+using namespace std;
+// for convenience
+//using json = nlohmann::json;
+
+int main(){
+	// string str;
+	// cin >> str;
+	// ifstream fin(str);
+	// //fin.open(str);
+	// string test;
+	// while(fin >> test){
+	// 	//string test;
+	// 	//string temp = test.substr(1 , test.length() - 2);
+	// 	cout << test << endl;	
+	// }
+
+	string str = "123";
+	int num = stoi(str);
+	cout << num << endl;
+
+	
+	
+}
+//cmd
+//g++ -std=c++11 test.cpp -o test
+  
+
